fa9716932b25b6620c5428dce39e3d5ecac73883
Pieter-Jan Drouillon <drouillon@hotmail.com>
2014-06-01 18:04:21 +0200

    Extended explanation in comments.

--- template.php
+++ template.php
@@ -4,7 +4,7 @@
 override theme_button().
 */
 function miniport_button($variables){
-  //add the button class
+  //add the button class defined by miniport theme
   $variables['element']['#attributes']['class'][] = 'button';
   //return the rendered button
   return theme_button($variables);
