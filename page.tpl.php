5cdf1f737d78fe830d39e15f2a03beb14d412b7c
Pieter-Jan Drouillon <drouillon@hotmail.com>
2014-06-02 20:56:13 +0200

    Added tabs.

--- page.tpl.php
+++ page.tpl.php
@@ -173,6 +173,21 @@
                       <?php print render($page['highlighted']); ?>
                     </div>
                   <?php endif; ?>
+                  
+                  <?php print render($title_prefix); ?>
+                  <?php if ($title): ?>
+                    <h1 class="title" id="page-title">
+                      <?php print $title; ?>
+                    </h1>
+                  <?php endif; ?>
+                  <?php print render($title_suffix); ?> 
+                  
+                  <?php if ($tabs): ?>
+                    <div class="tabs">
+                     <?php print render($tabs); ?>
+                    </div>
+                  <?php endif; ?> 
+                  
                   <?php print render($page['content']); ?>
                 </div>
               </article>
@@ -185,10 +200,25 @@
             <article>
               <div class="container">
                 <?php if ($page['highlighted']): ?>
-                    <div id="highlighted">
-                      <?php print render($page['highlighted']); ?>
-                    </div>
-                  <?php endif; ?>
+                  <div id="highlighted">
+                    <?php print render($page['highlighted']); ?>
+                  </div>
+                <?php endif; ?>
+                
+                <?php print render($title_prefix); ?>
+                <?php if ($title): ?>
+                  <h1 class="title" id="page-title">
+                    <?php print $title; ?>
+                </h1>
+                <?php endif; ?>
+                <?php print render($title_suffix); ?>  
+                
+                <?php if ($tabs): ?>
+                  <div class="tabs">
+                   <?php print render($tabs); ?>
+                  </div>
+                <?php endif; ?> 
+                  
                 <?php print render($page['content']); ?>
               </div>
             </article>
